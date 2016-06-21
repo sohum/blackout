c024fb11d5f6d3bc3dd2eb4068222eaf2a747819
shoumitra <shoumitra362@gmail.com>
2016-06-21 18:43:24 -0400

    SMS reading and number picker

--- blackout_demo/app/src/main/java/com/bitsplease/blackout_demo/DisplayObject.java
+++ blackout_demo/app/src/main/java/com/bitsplease/blackout_demo/DisplayObject.java
@@ -1,7 +1,29 @@
 package com.bitsplease.blackout_demo;
 
+import java.io.Serializable;
+
 /**
  * Created by sunny on 2016-06-18.
  */
-public class DisplayObject {
+public class DisplayObject implements Serializable {
+    private String text;
+    private String type;
+    private String time;
+
+    public DisplayObject(String text, String type, String time){
+        this.text = text;
+        this.type = type;
+        this.time = time;
+    }
+
+    public String getText(){
+        return this.text;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public String getTime(){
+        return this.time;
+    }
+
 }
--- blackout_demo/app/src/main/java/com/bitsplease/blackout_demo/MainActivity.java
+++ blackout_demo/app/src/main/java/com/bitsplease/blackout_demo/MainActivity.java
@@ -1,7 +1,23 @@
 package com.bitsplease.blackout_demo;
 
+import android.Manifest;
+import android.app.ListActivity;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.shawnlin.numberpicker.NumberPicker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +25,76 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //sohum loveguru
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_SMS},
+                    123);
+        }
+        else
+        {
+            NumberPicker numberPicker = (NumberPicker) findViewById(R.id.number_picker);
+            int value = numberPicker.getValue();
+            List<DisplayObject> smsInRange = getSMS();
+        }
+
 
     }
+
+    /**Method to get all sms in a time range */
+    public List<DisplayObject> getSMS(){
+
+        List<DisplayObject> smsList = new ArrayList<DisplayObject>();
+        Uri message = Uri.parse("content://sms/inbox");
+        ContentResolver cr = getContentResolver();
+        long currentTime = System.currentTimeMillis();
+        long range = TimeUnit.HOURS.toMillis(6);
+
+        Cursor c = cr.query(message, null, "date >= ?", new String[] { Long.toString(currentTime - range)}, null);
+        int totalSMS = c.getCount();
+
+        if (c.moveToFirst()) {
+            for (int i = 0; i < totalSMS; i++) {
+                DisplayObject sms = new DisplayObject(c.getString(c.getColumnIndexOrThrow("body")),
+                "SMS",c.getString(c.getColumnIndexOrThrow("date")));
+                smsList.add(sms);
+                c.moveToNext();
+            }
+        }
+         else {
+         throw new RuntimeException("You have no SMS in Sent");
+         }
+        c.close();
+        return smsList;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 123: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    List<DisplayObject> smsInRange = getSMS();
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
 }
