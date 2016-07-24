df32d7be55207aee86eca118556eeb23bf585718
Sohum <sohumrajguru@gmail.com>
2016-07-23 20:16:42 -0400

    Revert "Revert "Merge branch 'shomo' into sohum-UI""
    This reverts commit be781ec03f67c7ca6067532f645051d7d5519081.

--- blackout_demo/app/src/main/java/com/bitsplease/blackout_demo/ServiceFacade.java
+++ blackout_demo/app/src/main/java/com/bitsplease/blackout_demo/ServiceFacade.java
@@ -2,6 +2,11 @@ package com.bitsplease.blackout_demo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.util.Map;
 
 /**
  * Created by shoumitrasrivastava on 2016-07-21.
@@ -9,6 +14,7 @@ import android.content.Intent;
 public class ServiceFacade {
 
     private static final String EXTRA_PARAM1 = "com.bitsplease.blackout_demo.extra.PARAM1";
+    private static final String EXTRA_PARAM2 = "com.bitsplease.blackout_demo.extra.PARAM2";
 
     public ServiceFacade(){
 
@@ -16,14 +22,43 @@ public class ServiceFacade {
 
     public void startService(Context context, int hours){
 
+        boolean fb = false;
+        boolean twitter = false;
+        boolean sms = false;
+
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        Map<String,?> keys = prefs.getAll();
+
+        for(Map.Entry<String,?> entry : keys.entrySet())
+        {
+            if(entry.getKey().equals("facebook_switch")){
+                fb = Boolean.parseBoolean(entry.getValue().toString());
+            }
+            if(entry.getKey().equals("twitter_switch")){
+                twitter = Boolean.parseBoolean(entry.getValue().toString());;
+            }
+            if(entry.getKey().equals("sms_switch")){
+                sms =Boolean.parseBoolean(entry.getValue().toString());;
+            }
+
+        }
+
         Intent intentFacebook = new Intent(context, FacebookService.class);
         intentFacebook.setAction("com.bitsplease.blackout_demo.action.FACEBOOK");
+        intentFacebook.putExtra(EXTRA_PARAM2, fb);
         context.startService(intentFacebook);
 
         Intent intentSMS = new Intent(context, FacebookService.class);
         intentSMS.setAction("com.bitsplease.blackout_demo.action.SMS");
         intentSMS.putExtra(EXTRA_PARAM1, Integer.toString(hours));
+        intentSMS.putExtra(EXTRA_PARAM2, sms);
         context.startService(intentSMS);
+
+        Intent intentTwitter = new Intent(context, FacebookService.class);
+        intentTwitter.setAction("com.bitsplease.blackout_demo.action.TWITTER");
+        intentTwitter.putExtra(EXTRA_PARAM2, twitter);
+        context.startService(intentTwitter);
 
     }
 
